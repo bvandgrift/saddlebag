476c15a400bbb2b5555f9e38373a280e6eef9d69
Ben Vandgrift <ben@vandgrift.com>
2020-09-08 18:15:53 -0400

    adds webpacker, converts counter/index view to use javascript_pack_tag

--- app/helpers/saddlebag/application_helper.rb
+++ app/helpers/saddlebag/application_helper.rb
@@ -1,4 +1,11 @@
+require "webpacker/helper"
+
 module Saddlebag
   module ApplicationHelper
+    include ::Webpacker::Helper
+
+    def current_webpacker_instance
+      Saddlebag.webpacker
+    end
   end
 end
--- lib/saddlebag/engine.rb
+++ lib/saddlebag/engine.rb
@@ -1,5 +1,28 @@
 module Saddlebag
   class Engine < ::Rails::Engine
     isolate_namespace Saddlebag
+
+    initializer "webpacker.proxy" do |app|
+      insert_middleware = begin
+        Saddlebag.webpacker.config.dev_server.present?
+      rescue
+        nil
+      end
+      next unless insert_middleware
+
+      app.middleware.insert_before(
+        0, Webpacker::DevServerProxy, # "Webpacker::DevServerProxy" if Rails version < 5
+        ssl_verify_none: true,
+        webpacker: Saddlebag.webpacker
+      )
+
+      # use packs from saddlebag via Rack static
+      # file service?
+      config.app_middleware.use(
+        Rack::Static,
+        # note! this varies from the webpacker/engine documentation
+        urls: ["/saddlebag-packs"], root: Saddlebag::Engine.root.join("public")
+      )
+    end
   end
 end
